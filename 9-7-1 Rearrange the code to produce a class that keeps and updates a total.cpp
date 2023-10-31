ded151a344acd63330e0c105366830d87048d7ec
mikeLTR <m.osp@hotmail.com>
2023-10-31 12:17:30 +0300

    #first code

---
+++ 9-7-1 Rearrange the code to produce a class that keeps and updates a total.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+	Car();
+	double get_odometer() const;
+	void drive(double miles);
+
+private:
+	double odometer;
+};
+
+double Car :: get_odometer() const
+{
+	return odometer;
+}
+void Car::drive(double miles)
+{
+	odometer = odometer + miles;
+}
+Car::Car()
+{
+	odometer = 0;
+}
+int main()
+{
+	Car car;
+	cout << "Odometer before trip: ";
+	cout << car.get_odometer() << endl;
+
+	double miles;
+	while (cin >> miles)
+	{
+		car.drive(miles);
+		cout << "odometer after" << miles << "miles: ";
+		cout << car.get_odometer() << endl;
+	}
+	return 0;
+}
