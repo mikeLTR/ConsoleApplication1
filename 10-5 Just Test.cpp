3aa2057b598f48353532c59fb7fbf55a92384c56
mikeLTR <m.osp@hotmail.com>
2023-11-24 18:48:44 +0300

    # first version

---
+++ 10-5 Just Test.cpp
@@ -0,0 +1,8 @@
+Question* quiz[2];
+quiz[0] = new Question;
+quiz[0]->set_text("who was the onventor of C++");
+quiz[0]->set_unswer(:Bjarne Stroustrup");
+	ChoiseQuestion * cq_pointer = new ChoiseQuestion;
+cq_pointer_ > set_text("In whicj country was the inventor of C++ bortn");
+cq_pointer->add_choice("Australia", false);
+qiuze[1] = cq_pointer;
